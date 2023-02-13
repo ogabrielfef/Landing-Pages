5bbbb08cd71a0f3e9aabc9b242ab96c1ca17bd9a
GabrielF <ogabrielfef@gmail.com>
2023-02-13 11:57:20 -0300

    funcionalidade rotativa das img

---
+++ The Last of Us Page/script.js
@@ -0,0 +1,21 @@
+let time = 2000;
+let images = document.querySelectorAll('#slider img');
+let currentImage = 0;
+let max = images.length;
+
+function nextImage() {
+  images[currentImage].classList.remove('selected')
+  currentImage++;
+  if (currentImage >= max) {
+    currentImage = 0;
+  }
+  images[currentImage].classList.add('selected')
+}
+
+function play() {
+  setInterval(() => {
+    nextImage();
+  }, time)
+}
+
+window.addEventListener('load', play);
